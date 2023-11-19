4c9912e344e106c66be4f4cee946ee2575dd77db
Ben C <bwc9876@gmail.com>
2023-11-19 16:46:52 -0500

    Suspend on lid close

--- modules/greeter.nix
+++ modules/greeter.nix
@@ -8,12 +8,14 @@
     enable = true;
     settings = {
       default_session = let
+        greeting = ''--greeting "Authenticate into ${lib.toUpper hostName}"'';
         wlr_setting =
           if hostName == "b-pc-tower"
           then "WLR_NO_HARDWARE_CURSORS=1"
           else "WLR_NO_HARDWARE_CURSORS=0";
+        cmd = ''--cmd "systemd-inhibit --what=handle-power-key:handle-lid-switch sh -c \"${wlr_setting} Hyprland\""'';
       in {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --remember --greeting \"Authenticate into ${lib.toUpper hostName}\" --time --cmd \"systemd-inhibit --what=handle-power-key:handle-lid-switch sh -c \\\"${wlr_setting} Hyprland\\\"\"";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --remember --time ${greeting} ${cmd}";
       };
     };
   };
