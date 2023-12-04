6da3eac5b6243b6a71141d7fd61d92441663950e
Ben C <bwc9876@gmail.com>
2023-12-04 14:33:38 -0500

    Fix Zoxide temporarily

--- home/shell.nix
+++ home/shell.nix
@@ -6,7 +6,17 @@
   programs = {
     command-not-found.enable = false;
     nix-index.enable = true;
-    zoxide.enable = true;
+    zoxide = {
+      enable = true;
+      package = pkgs.callPackage pkgs.zoxide.overrideAttrs {
+        src = pkgs.fetchFromGitHub {
+          owner = "ajeetdsouza";
+          repo = "zoxide";
+          rev = "3022cf3686b85288e6fbecb2bd23ad113fd83f3b";
+          sha256 = "sha256-ut+/F7cQ5Xamb7T45a78i0mjqnNG9/73jPNaDLxzAx8=";
+        };
+      };
+    };
     ripgrep.enable = true;
 
     starship = {
