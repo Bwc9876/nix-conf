5495626c18bdb9edbf6b42afc99759ea6c4ea6b2
Ben C <bwc9876@gmail.com>
2024-04-11 11:33:52 -0400

    Add Laptop Pubkey For Tower

--- computers/b-pc-tower/ssh.nix
+++ computers/b-pc-tower/ssh.nix
@@ -1,6 +1,11 @@
 {
     ...
 }: {
+
+    users.users."bean".openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKb2qxNUbvdBTAntmUyPIaOXwFd1nhZO/SS00SNss0nU bean@B-PC-LAPTOP"
+    ];
+
     services.openssh = {
         enable = true;
         ports = [ 8022 ];
